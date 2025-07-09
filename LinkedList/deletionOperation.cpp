c15d59b7083655d720dd14b2daf6b8a4fb7299f1
Prachichougale30 <prachichougale2530@gmail.com>
2025-07-09 21:06:46 +0530

    delete node

--- LinkedList/deletionOperation.cpp
+++ LinkedList/deletionOperation.cpp
@@ -26,14 +26,14 @@ struct Node
 };
 void deleteNode(int position, Node *&head)
 {
-    // deleting first or strat node
+    // deleting first or start node
     if (position == 1)
     {
         Node *temp = head;
         head = head->next;
         // memory free start node;
-        temp->next=NULL;
-        delete temp; // destructore create
+        temp->next = NULL;
+        delete temp; // destructor create
     }
     else
     {
@@ -41,36 +41,44 @@ void deleteNode(int position, Node *&head)
         Node *curr = head;
         Node *prev = NULL;
         int cnt = 1;
-        while (cnt <= position)
+        while (cnt < position)
         {
             prev = curr;
             curr = curr->next;
             cnt++;
         }
         prev->next = curr->next;
+        curr->next = NULL;
         delete curr;
     }
 }
-void print(Node* &head) {
+void print(Node *&head)
+{
 
-    if(head == NULL) {
-        cout << "List is empty "<< endl;
-        return ;
+    if (head == NULL)
+    {
+        cout << "List is empty " << endl;
+        return;
     }
-    Node* temp = head;
+    Node *temp = head;
 
-    while(temp != NULL ) {
-        cout << temp -> data << " ";
-        temp = temp -> next;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
     cout << endl;
 }
-int main(){
-    Node* node1=new Node(10);
-   // cout<<node1->data<<endl;
+int main()
+{
+    Node *node1 = new Node(10);
+    // cout<<node1->data<<endl;
 
-    Node*head=node1;
-
-    deleteNode(1,head);
+    Node *head = node1;
+    node1->next = new Node(20);
+    node1->next->next = new Node(30);
+    node1->next->next->next = new Node(40);
+    deleteNode(1, head);
+    deleteNode(2, head);
     print(head);
 }
