f19103812105207a30a18f277c4510f3442395ff
Prachichougale30 <prachichougale2530@gmail.com>
2025-08-05 21:07:52 +0530

    valid paranthesies added

---
+++ Stack/validParanthesis.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isValidParanthesis(string expression)
+{
+    stack<char> s;
+
+    for (int i = 0; i < expression.length(); i++)
+    {
+        char ch = expression[i];
+
+        // If opening bracket, push to stack
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            s.push(ch);
+        }
+        else
+        {
+            // Closing bracket
+            if (!s.empty())
+            {
+                char top = s.top();
+                if ((ch == ')' && top == '(') ||
+                    (ch == '}' && top == '{') ||
+                    (ch == ']' && top == '['))
+                {
+                    s.pop();
+                }
+                else
+                {
+                    return false; // Mismatched pair
+                }
+            }
+            else
+            {
+                return false; // Stack empty but closing bracket found
+            }
+        }
+    }
+
+    return s.empty(); // True if all brackets matched
+}
+int main() {
+    string exp1 = "{[()]}";
+    string exp2 = "{[(])}";
+
+    cout << isValidParanthesis(exp1) << endl; // 1 (true)
+    cout << isValidParanthesis(exp2) << endl; // 0 (false)
+
+    return 0;
+}
