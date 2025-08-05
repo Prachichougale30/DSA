4aa9cf3ce636d7050cdb78d840c6dd7fca1ec383
Prachichougale30 <prachichougale2530@gmail.com>
2025-08-05 20:28:53 +0530

    delete middle from stack added

---
+++ Stack/deleteMiddleFromStack.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(stack<int> &inputStack, int size, int count)
+{
+    // recursion
+    if (count == size / 2)
+    {
+        inputStack.pop();
+        return;
+    }
+    int num = inputStack.top();
+    inputStack.pop();
+    // recursive call
+    solve(inputStack, size, count + 1);
+    inputStack.push(num);
+}
+void deleteMiddle(stack<int> &inputStack, int N)
+{
+
+    // Write your code here
+    int count = 0;
+    solve(inputStack, N, count);
+}
