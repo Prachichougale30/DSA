f70a39efe1e0ae36b26fc51057c1fc544ef5d231
Prachichougale30 <prachichougale2530@gmail.com>
2025-10-08 21:04:51 +0530

    238. Product of Array Except Self added

---
+++ Array/238. Product of Array Except Self.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+            int n = nums.size();
+             // Vector to store all left multiplications 
+           vector<int> left(n);
+             // Vector to store all right multiplications
+           vector<int> right(n);
+
+           // Calculate left products
+    left[0] = 1;
+    for (int i = 1; i < n; i++) {
+        left[i] = left[i - 1] * nums[i - 1];
+    }
+
+    // Calculate right products
+    right[n - 1] = 1;
+    for (int i = n - 2; i >= 0; i--) {
+        right[i] = right[i + 1] * nums[i + 1];
+    }
+    vector<int> ans(n);
+    for (int i = 0; i < n; i++) {
+        ans[i] = left[i] * right[i];
+    }
+
+    return ans;
+
+    }
+};
