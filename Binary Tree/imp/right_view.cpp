a9d502762edbaa699d1035ddfcf16cab4e52a5a6
Prachichougale30 <prachichougale2530@gmail.com>
2025-10-15 19:42:45 +0530

    left and right view of BT added

---
+++ Binary Tree/imp/right_view.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> leftView(Node *root) {
+        vector<int> ans;
+        if (root == NULL) {
+            return ans;
+        }
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+            // Traverse one level
+            for (int i = 0; i < n; i++) {
+                Node* frontNode = q.front();
+                q.pop();
+
+                // The first node of every level is part of the right view
+                if (i == n-1) {
+                    ans.push_back(frontNode->data);
+                }
+
+                if (frontNode->left) {
+                    q.push(frontNode->left);
+                }
+                if (frontNode->right) {
+                    q.push(frontNode->right);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
