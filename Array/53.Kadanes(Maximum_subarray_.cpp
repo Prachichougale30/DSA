0cbf2b52f53f2432297032a675855153b447e5ce
Prachichougale30 <prachichougale2530@gmail.com>
2026-01-23 23:51:52 +0530

    Maximum subarray

---
+++ Array/53.Kadanes(Maximum_subarray_.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+
+    public:
+    int maxSubarray(vector<int>&nums){
+         int currSum=0,maxSum=INT_MIN;
+
+      for(int val:nums){
+            currSum+=val;
+            maxSum=max(currSum,maxSum);
+
+            if(currSum<0){
+                currSum=0;
+            }
+      } 
+      return maxSum;
+}
+    };
+int main(){
+
+
+}
---
+++ Binary Tree/imp/K_Sum_paths.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+class Solution {
+  public:
+  
+    void solve(Node*root,int k,int &count,vector<int> path){
+        if(root==NULL){
+            return;
+        }
+        path.push_back(root->data);
+        
+        solve(root->left,k,count,path);
+        
+        solve(root->right,k,count,path);
+        
+        // check for k sum
+        
+        int size=path.size();
+        int sum=0;
+        for(int i=size-1;i>=0;i--){
+            sum+=path[i];
+            if(sum==k){
+                count++;
+            }
+            
+        }
+        path.pop_back();
+    }
+    
+    int countAllPaths(Node *root, int k) {
+        // code here
+        vector<int> path;
+        int count=0;
+        solve(root,k,count,path);
+        
+        return count;
+    }
+};
