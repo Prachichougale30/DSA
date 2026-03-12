53efd1cb84e09b3fd86a8f4c08af7e5f22eab559
Prachichougale30 <prachichougale2530@gmail.com>
2026-03-12 12:28:32 +0530

    Add initial implementation for cycle detection in undirected graph

---
+++ Graph/Cycle_Detection_in_Undirected_Graph.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isCyclicBFS(int src, unordered_map<int,bool> &visited,
+                 unordered_map<int,list<int>> &adj)
+{
+    unordered_map<int,int> parent;
+
+    parent[src] = -1;
+    visited[src] = true;
+
+    queue<int> q;
+    q.push(src);
+
+    while(!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+
+        for(auto neighbour : adj[front])
+        {
+            if(visited[neighbour] && neighbour != parent[front])
+            {
+                return true;
+            }
+            else if(!visited[neighbour])
+            {
+                q.push(neighbour);
+                visited[neighbour] = true;
+                parent[neighbour] = front;
+            }
+        }
+    }
+
+    return false;
+}
+
+string cycleDetection(vector<vector<int>> &edges, int n, int m)
+{
+    unordered_map<int,list<int>> adj;
+
+    // create adjacency list
+    for(int i = 0; i < m; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    unordered_map<int,bool> visited;
+
+    // handle disconnected components
+    for(int i = 0; i < n; i++)
+    {
+        if(!visited[i])
+        {
+            bool ans = isCyclicBFS(i, visited, adj);
+            if(ans)
+                return "Yes";
+        }
+    }
+
+    return "No";
+}
